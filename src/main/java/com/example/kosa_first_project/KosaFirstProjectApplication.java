f9fc8a5e3475d1113b451091b8395b4ce288dad9
Dejavu-Jamaisvu <122339479+Dejavu-Jamaisvu@users.noreply.github.com>
2024-10-13 19:02:46 +0900

    1013설정완료

--- src/main/java/com/example/kosa_first_project/KosaFirstProjectApplication.java
+++ src/main/java/com/example/kosa_first_project/KosaFirstProjectApplication.java
@@ -2,8 +2,9 @@ package com.example.kosa_first_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
 public class KosaFirstProjectApplication {
 
     public static void main(String[] args) {
