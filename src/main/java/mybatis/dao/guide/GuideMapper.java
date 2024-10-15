1752071111a79aa82f4a40f2dbcf32764835f033
REVE <aiwork4242@gmail.com>
2024-10-15 12:28:39 +0900

    #3 DTO 작성

--- src/main/java/mybatis/dao/guide/GuideMapper.java
+++ src/main/java/mybatis/dao/guide/GuideMapper.java
@@ -3,6 +3,7 @@ package mybatis.dao.guide;
 
 import com.example.kosa_first_project.domain.guide.GuideInfoDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -11,19 +12,19 @@ public interface GuideMapper {
 
     public interface GuideInfoMapper {
 
-        // Insert a new guide info
+
         void insertGuideInfo(GuideInfoDTO guideInfo);
 
-        // Retrieve guide info by ID
+
         GuideInfoDTO getGuideInfoById(int id);
 
-        // Retrieve all guide info entries
+        //@Select()
         List<GuideInfoDTO> getAllGuideInfo();
 
-        // Update an existing guide info
+
         void updateGuideInfo(GuideInfoDTO guideInfo);
 
-        // Delete a guide info by ID
+
         void deleteGuideInfo(int id);
     }
 
