c3572abdd92835b39a8717a121d6bbd6b3637564
REVE <aiwork4242@gmail.com>
2024-10-16 21:54:06 +0900

    #4 guide_form.html 수정 - form 태그

--- src/main/java/com/example/kosa_first_project/controller/guide/GuideFormController.java
+++ src/main/java/com/example/kosa_first_project/controller/guide/GuideFormController.java
@@ -3,6 +3,9 @@ package com.example.kosa_first_project.controller.guide;
 import com.example.kosa_first_project.domain.guide.GuideInfoDTO;
 import com.example.kosa_first_project.service.guide.GuideFormService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -13,9 +16,31 @@ public class GuideFormController {
     @Autowired
     private GuideFormService guideFromService;
 
-    @PostMapping("/add")
-    public String addGuide(@RequestBody GuideInfoDTO guideDTO) {
-        guideFromService.saveGuideInfo(guideDTO);
-        return "가이드 정보가 저장되었습니다.";
+
+    @PostMapping("/guides")
+    public String saveGuide(@ModelAttribute GuideInfoDTO guideInfoDTO, Model model) {
+        // Save the guide information to the database using guideDTO
+
+        return "redirect:/guide_list.html";
     }
+
+
+    /*
+    @PostMapping("/guides")
+    public String addGuide(@RequestBody GuideInfoDTO guideinfoDTO) {
+        guideFromService.saveGuideInfo(guideinfoDTO);
+        return "가이드 정보가 저장되었습니다.";
+    }*/
+
+
+    /*
+    @PostMapping
+    public ResponseEntity<String> addGuide(@RequestBody GuideInfoDTO guideinfoDTO) {
+        try {
+            guideFromService.saveGuideInfo(guideinfoDTO);
+            return ResponseEntity.ok("Guide saved successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving guide");
+        }
+    }*/
 }
