7af1cd0d70217c66955f4984e594413ed7903deb
REVE <aiwork4242@gmail.com>
2024-10-20 16:10:20 +0900

    #5 경로 수정

--- src/main/java/com/example/kosa_first_project/controller/guide/TestGuideController.java
+++ src/main/java/com/example/kosa_first_project/controller/guide/TestGuideController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestGuideController {
 
     //guide 의 각 기능을 테스트 하기위한 호출 Controller //수정해서 사용
-    @GetMapping("/guide")
+    @GetMapping("/mypage")
     public String myPage(Model model) {
         // 필요한 데이터 모델에 추가
         // model.addAttribute("message", "Hello, World!");
@@ -16,6 +16,6 @@ public class TestGuideController {
         //return "redirect:/guide/mypage_guide_list.html"; // 리다이렉트 : 파일이 static 아래에 있어야함
         //return "guide/mypage_guide_list"; //templates 아래에 있으며, url이 바뀌지 않음.
 
-        return "guide/guide_card";
+        return "guide/mypage_guide_list";
     }
 }
