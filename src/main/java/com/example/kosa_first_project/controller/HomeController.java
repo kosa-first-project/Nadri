62bacf8caf00c9577c18b342b7edfbd3ea62bfa0
REVE <aiwork4242@gmail.com>
2024-10-20 14:57:06 +0900

    #1 메인 홈화면 URL 설정 - 보류 (controller로 부르는 방식의 코드)

--- src/main/java/com/example/kosa_first_project/controller/HomeController.java
+++ src/main/java/com/example/kosa_first_project/controller/HomeController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//템플릿아래에 메인화면이 있을 때 사용할 클래스.
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/home") // /로 해놓으면 됨. 일단 home으로 변경해놓음.
     public String myPage(Model model) {
-        return "martine/index"; //templates 아래에 있으며, url이 바뀌지 않음.
+        return "martine/index";
     }
 }
