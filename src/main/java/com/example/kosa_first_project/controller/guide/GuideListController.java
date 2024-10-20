4171db3a4e9e6ba12c9d8cda3391572c485fbe39
REVE <aiwork4242@gmail.com>
2024-10-20 15:12:52 +0900

    #5 #6 #7 경로 수정

--- src/main/java/com/example/kosa_first_project/controller/guide/GuideListController.java
+++ src/main/java/com/example/kosa_first_project/controller/guide/GuideListController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/guides")
-public class MyPageGuideListController {
+@RequestMapping("/guides/list")
+public class GuideListController {
 
     @Autowired
     private MyPageGuideListService guideListService;
